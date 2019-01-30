81dff9c8b1415165b4b4e91e18161bd1a6fa1369
shixiaodong <shixiaodong@songshuai.com.cn>
2019-01-30 15:44:47 +0800

    test

--- aa/src/main/java/com/example/aa/controler/Hello.java
+++ aa/src/main/java/com/example/aa/controler/Hello.java
@@ -1,5 +1,6 @@
 package com.example.aa.controler;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Hello {
 
 
-        @PostMapping("/reg")
+        @GetMapping("/reg")
         @ResponseBody
         public Object reg(){
 
